2c712183450aef0b97a89972f6d1193f4094f122
Iaan van Niekerk <iaan@plusplusminus.co.za>
2016-03-24 09:36:16 +0200

    contact icon

--- wp-content/themes/kvd/templates/module/page/header_child.php
+++ wp-content/themes/kvd/templates/module/page/header_child.php
@@ -1,8 +1,7 @@
 <header class="page-header panel-heading">
 	<div class="page-header__content panel-title">
-		<?php $post->ID == 660 ? $class="down" : $class="up"; ?>
 		<a data-toggle="collapse" class="collapsed" href="#acc<?php echo $post->ID; ?>" aria-expanded="true" aria-controls="<?php // echo $title; ?>">
-			<h3 class="page-header__title"><?php the_title(); ?> <i class="icon <?php echo $class; ?> icon-accordionarrow"></i></h3>
+			<h3 class="page-header__title"><?php the_title(); ?> <i class="icon up icon-accordionarrow"></i></h3>
 		</a>
 	</div>
 </header>
